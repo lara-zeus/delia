79357a8e82e0ead9986b0bfe7e0b8bd84319b88b
Ash Monsh <wh7r.com@gmail.com>
2026-04-16 22:11:05 +0300

    support filament 5

--- src/DeliaServiceProvider.php
+++ src/DeliaServiceProvider.php
@@ -14,7 +14,7 @@ class DeliaServiceProvider extends PackageServiceProvider
 
     public function packageBooted(): void
     {
-        Livewire::component('delia-bookmarks', BookmarkComponent::class);
+        Livewire::addComponent(name:'delia-bookmarks',class: BookmarkComponent::class);
     }
 
     public function configurePackage(Package $package): void
